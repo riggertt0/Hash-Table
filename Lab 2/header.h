435c976d2acd6c70fbe6c5f264918864f8c5ab0a
riggertt0 <70891530+riggertt0@users.noreply.github.com>
2021-05-18 16:57:20 +0300

    Create header.h

---
+++ Lab 2/header.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+
+#include <immintrin.h>
+#include <xmmintrin.h>
+#include <nmmintrin.h>
+
+const int TABLE_SIZE = 1009;
+const int N_WORDS = 24000;
+const int N_SAMPLES = 8000000;
+const int WORD_SIZE = 48;
+
+const char* WORD_SET = "vocabulary.txt";
+const char* LOG_PATH = "test.txt";
