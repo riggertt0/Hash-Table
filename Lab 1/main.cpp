b4be2793d3aa5edccbe0db5e353a7201435a3548
riggertt0 <70891530+riggertt0@users.noreply.github.com>
2021-05-18 16:51:59 +0300

    Delete main.cpp

--- Lab 1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "HashTable.h"
-
-int main()
-{
-    HashTable table (400, CRC32Hash);
-    // table.table_[0] = new Elem {"123", 3, "456", 4};
-    table.FillTable ("../Dicts/UTF-dict1.txt");
-    table.GetStatistic ();
-}
